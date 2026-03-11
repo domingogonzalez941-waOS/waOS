ccb5f181dd8302419e7f8dbbc4270f0f134eeeae
SNOW <domingogonzalez941@gmail.com>
2026-03-11 14:10:41 -0400

    Separamos aun mas el codigo creando dos archivos nuevos .js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                mono: ['Fira Code', 'monospace'],
+                display: ['Space Grotesk', 'sans-serif'],
+            },
+            colors: {
+                acurast: '#c4ff00',
+                dark: '#030303',
+                bitcoin: '#F7931A', // Añadido color Bitcoin
+            }
+        }
+    }
+}
